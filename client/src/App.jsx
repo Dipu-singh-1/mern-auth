f1b40ac1b82167bedfc595a639373f41b05e6d40
dipu singh <dipusingh8294935492@gmail.com>
2023-12-22 13:01:05 +0530

    bug fix(in PrivateRoute.jsx & App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
-import PrivateRoute from "./components/Privateroute";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
